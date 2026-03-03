7f56fa51a70e19cb3d903e5719ceba6236fc688d
drajhari4418 <39647887+drajhari4418@users.noreply.github.com>
2026-03-03 13:05:20 +0530

    updated

--- main-project-work-on/script/main_analysis.R
+++ main-project-work-on/script/main_analysis.R
@@ -1,6 +1,48 @@
 #PACKAGES REQUIRED-----
-if(!require(pacman))install.packages("pacman")
-pacman::p_load(tidyverse, inspectdf,plotly, janitor, visdat, esquisse)
+
+#if(!require(pacman))install.packages("pacman")
+#pacman::p_load(tidyverse, inspectdf,plotly, janitor, visdat, esquisse,here)
+
+
 #load data----
-ebola_sierra_leone <- read_csv("data/ebola_sierra_leone.csv")
+library(tidyverse)
+library(inspectdf)
+library(plotly)
+library(janitor)
+library(visdat)
+library(esquisse)
+library(here)
+ebola_sierra_leone <- read_csv(here("data/ebola_sierra_leone.csv"))
+#DATA EXPLORATION(cases under district)-----
+district_tab <- tabyl(ebola_sierra_leone, district)
+district_tab
+
+write.csv(x = district_tab, file = "outputs/district tabulation.csv")
+#DATA EXPLORATION(in a categorical variables)-----
+cat_summary <- inspect_cat(ebola_sierra_leone)
+
+categorical_variables_plot <- ggplot(cat_summary,
+                                     aes(x = col_name, y = cnt)) +
+  geom_col(fill = "#112446")
+
+ggplotly(categorical_variables_plot)
+ggsave(categorical_variables_plot , filename = "outputs/categorical variables plot.png")
+#DATA EXPLORATION(in a numerical value)-----
+
+
+#num_summary <- inspect_num(ebola_sierra_leone)
+#ggsave(num_summary , filename = "outputs/numerical variables plot.png")
+#num_summary_plot <- ggplot(num_summary,
+#                           aes(x = col_name, y = cnt)) +
+#  geom_col(fill = "#112446")
+#ggplotly(num_summary_plot)
+
+
+p <- ggplot(ebola_sierra_leone,
+            aes(x = age, fill = status)) +
+  geom_histogram(bins = 30, position = "identity", alpha = 0.6) +
+  theme_minimal()
+
+ggplotly(p)
+
 
